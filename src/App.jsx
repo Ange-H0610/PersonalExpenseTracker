dd999e24b89afcba697cad9da3efa7cc2f94ad69
Antenaina012 <hei.antenaina.7@gmail.com>
2025-09-11 23:31:59 +0300

    feat: added path.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import { useState } from "react";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import LoginForm from "./login";
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import Homepage from "./Homepage";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <LoginForm />
+
+        <Router>
+          <Routes>
+            <Route path="/" element={<LoginForm />} />
+            <Route path="/homepage" element={<Homepage />} />
+          </Routes>
+        </Router>
   );
 }
 
