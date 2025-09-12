ee9b1c2daf403a29f4d73b696eafba31c05c2cc9
Soa <hei.soa.3@gmail.com>
2025-09-12 14:55:14 +1300

    ajout du bouton sign in

--- src/login.jsx
+++ src/login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./App.css";
 
 function LoginForm() {
@@ -33,24 +33,28 @@ function LoginForm() {
   return (
     <>
       <div
-        className="min-h-screen flex items-center w-full justify-center bg-gradient-to-r from-[#341c51] to-gray-800 via-[#471235]"
+        className="min-h-screen flex items-center w-full 
+      justify-center bg-gradient-to-r from-[#341c51]
+       to-gray-800 via-[#471235]"
       >
-        <article className="absolute ml-[-600px] text-3xl mb-5 font-bold text-balance ">
+        <article
+          className="absolute ml-[-600px] 
+        text-3xl mb-5 font-bold text-balance "
+        >
           <h1 className="mb-1.5 text-[#113257]">MONEFY</h1>
-          <h2 className="text-balance text-[#ACACAC]">Never lose track of your expenses !</h2>
+          <h2 className="text-balance text-[#ACACAC]">
+            Never lose track of your expenses !
+          </h2>
         </article>
 
-        <div
-          className="pt-[10px] mt-[30px] ml-[500px] bg-[#113257] w-[250px] h-[440px] rounded-[7px] text-white font-bold"
-        >
+        <div className="pt-[10px] mt-[30px] ml-[500px]
+         bg-[#113257] w-[250px] h-[440px] rounded-[7px] text-white font-bold">
           REGISTRATION
           <p className="pt-[35px] text-balance font-light ">
             Chose to track and how you want to balance your expenses .
           </p>
           <p className="font-light">It starts by login </p>
-          <div
-            className="bg-[#E9ECEE] mt-[20px] w-[250px] h-[280px] rounded-2xl"
-          >
+          <div className="bg-[#E9ECEE] mt-[20px] w-[250px] h-[315px] rounded-2xl">
             <form className="space-y-4" onSubmit={handleLogin}>
               <div>
                 <input
@@ -58,7 +62,8 @@ function LoginForm() {
                   placeholder="Enter your email"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  className="w-[200px] mt-2.5 text-black p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                  className="w-[200px] mt-2.5 text-black p-2 border rounded-lg
+                   focus:outline-none focus:ring-2 focus:ring-blue-400"
                 />
               </div>
 
@@ -68,16 +73,32 @@ function LoginForm() {
                   placeholder="Enter your password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  className="w-[200px] mt-2.5 p-2 text-black border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                  className="w-[200px] mt-2.5 p-2 text-black border
+                   rounded-lg focus:outline-none focus:ring-2
+                    focus:ring-blue-400"
                 />
               </div>
 
               <button
                 type="submit"
-                className="w-[150px] bg-[#1a384d] text-white py-3 rounded-lg hover:bg-[#6E9FC1] transition"
+                className="w-[150px] bg-[#1a384d]
+                 text-white py-3 rounded-lg 
+                 hover:bg-[#6E9FC1] transition"
               >
                 LOG IN
               </button>
+              <p className=" text-black
+               hover:text-gray-600 ">
+                {" "}
+                Forgot password ?
+              </p>
+              
+              <p className=" m
+               text-black text-sm font-light ">Don't have an account yet? 
+                
+                </p>
+                <button className=" mb-[15px] mt-[-25px] 
+                text-[#113257] hover:text-black " >Sign in </button>
             </form>
           </div>
         </div>
